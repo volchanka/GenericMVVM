ef8e0b2bd82f09460beff5252685c233ec017657
Svetlana <sma.aeris@gmail.com>
2019-05-13 20:47:43 +0300

    Initial Commit

---
+++ GenericMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericMVVM
+//
+//  Created by Svetlana on 13/05/2019.
+//  Copyright © 2019 ws233. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
